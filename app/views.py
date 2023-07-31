c397aa11154db17fe94a4b9b97b5e3561f194f62
pritKP234 <pritamkpatra@gmail.com>
2023-07-31 20:12:09 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def data_render(request):
     d={'name':'PRITAM','age':23}
     return render(request, 'data_render.html', context=d)
+
+def condition(request):
+    dict={'a':1000,'b':200, 'c':300}
+    return render(request, 'condition.html', context=dict)
